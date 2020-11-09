c8fa6edd62af5861d44e130ed50a56299305a99e
Emad Ghaffari <emadghaffariii@gmail.com>
2020-11-09 05:12:18 +0000

    start application files

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "github.com/emadghaffari/grpc_kafka_example/app"
+import (
+	"github.com/emadghaffari/grpc_kafka_example/app"         // start application
+	_ "github.com/emadghaffari/grpc_kafka_example/bootstrap" // bootstrap for configs
+)
 
-func main()  {
+func main() {
 	app.StartApplication()
 }
